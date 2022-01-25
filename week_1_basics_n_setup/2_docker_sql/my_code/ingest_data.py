e89db540d4ad85b02fdcd1c497e626fe9b59d0e8
Daniel J <danielj@Daniels-MacBook-Air.local>
2022-01-24 21:55:50 -0600

    Added docker-compose file

--- week_1_basics_n_setup/2_docker_sql/my_code/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/my_code/ingest_data.py
@@ -25,6 +25,7 @@ def main(params):
 
     df = next(df_iter)
 
+#remove for adding zone lookup table -- doesn't contain these columns
 #    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
 #    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
@@ -63,75 +64,3 @@ if __name__ == '__main__':
 
     main(args)
 
-
-""" #!/usr/bin/env python
-# coding: utf-8
-
-import pandas as pd
-import argparse
-import os
-import urllib
-
-from sqlalchemy import create_engine
-from time import time
-
-import argparse
-
-def main(params):
-    user = params.user
-    password = params.password
-    host = params.host
-    port = params.port
-    db = params.db
-    table_name = params.table_name
-    url = params.url
-    csv_name = 'output.csv'
-    
-    os.system(f"wget {url} -O {csv_name}")
-    
-    #h = urllib.urlretrieve(url, csv_name)
-
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
-
-    df = next(df_iter)
-
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
-    while True:
-        t_start = time()
-        
-        df = next(df_iter)
-        
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-        
-        df.to_sql(name=table_name, con=engine, if_exists='append')
-        
-        t_end = time()
-        
-        print(f'Inserted another chunk... took {t_end - t_start}')
-
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres.')
-
-    parser.add_argument('--user', help='username for postgres')
-    parser.add_argument('--password', help='password for postgres')
-    parser.add_argument('--host', help='host for postgres')                    
-    parser.add_argument('--port', help='port for postgres')
-    parser.add_argument('--db', help='database name for postgres')
-    parser.add_argument('--table_name', help='name of table where we will write results in postgres')
-    parser.add_argument('--url', help='url of the csv file')
-
-    args = parser.parse_args()
-
-    main(args)
-
- """
